6805088220b709217594c843841a0b58c2a979c0
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-12-08 01:54:53 +0200

    movies landing page

---
+++ index.js
@@ -0,0 +1,21 @@
+// fetch images of movies from imdb api
+fetch('https://imdb8.p.rapidapi.com/title/get-images', {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-rapidapi-key": "840f98efd7msh50e7d13ab092952p1ac739jsnf13a5333fb18",
+          "x-rapidapi-host": "imdb8.p.rapidapi.com"
+        }
+      })
+    .then(function(response) {  
+        return response.json();
+    }).then(function(data) {
+        // console.log(data.images);
+        var html = ""; 
+        $.each(data.images, function(item) {
+            html += "<div class='row'>";
+            html += "<img src='" + item.url + "' alt='image'  />";
+            html += "</div>";
+        });
+        $('#images-of-movies').html(html);
+    });
